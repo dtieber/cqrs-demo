989fb41e85c92d07f88ef21c49089630b083b75e
Daniel Tieber <daniel.tieber@redbull.com>
2020-11-15 14:28:59 +0100

    chore: disable max-length rules for header and body (commitlint)

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  extends: ['@commitlint/config-conventional']
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'body-max-line-length': [0, 'always', 100], // 0 = disable this rule
+    'header-max-length': [0, 'always', 100], // 0 = disable this rule
+  },
 }
